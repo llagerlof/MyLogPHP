dcd3776aa263f25c7f2a1407c83d2d5833a3f5f7
Lawrence Lagerlof <lawrence@ead.fiocruz.br>
2017-06-22 11:43:59 -0300

    New method out() allow the user to output variable content to a file.

--- MyLogPHP.class.php
+++ MyLogPHP.class.php
@@ -45,7 +45,7 @@ class MyLogPHP {
 		$this->LOGFILENAME = $logfilename;
 		$this->SEPARATOR = $separator;
 		$this->HEADERS =
-			'DATETIME' . $this->SEPARATOR . 
+			'DATETIME' . $this->SEPARATOR .
 			'ERRORLEVEL' . $this->SEPARATOR .
 			'TAG' . $this->SEPARATOR .
 			'VALUE' . $this->SEPARATOR .
@@ -55,7 +55,7 @@ class MyLogPHP {
 
 	/**
 	 * Private method that will write the text messages into the log file.
-	 * 
+	 *
 	 * @param string $errorlevel There are 4 possible levels: INFO, WARNING, DEBUG, ERROR
 	 * @param string $value The value that will be recorded on log file.
 	 * @param string $tag Any possible tag to help the developer to find specific log messages.
@@ -88,11 +88,11 @@ class MyLogPHP {
 
 	/**
 	 * Function to write non INFOrmation messages that will be written into $LOGFILENAME.
-	 * 
+	 *
 	 * @param string $value
-	 * @param string $tag 
+	 * @param string $tag
 	 */
-	function info($value = '', $tag = self::DEFAULT_TAG) {
+	public function info($value = '', $tag = self::DEFAULT_TAG) {
 
 		self::log('INFO', $value, $tag);
 	}
@@ -102,11 +102,11 @@ class MyLogPHP {
 	 *
 	 * Warning messages are for non-fatal errors, so, the script will work properly even
 	 * if WARNING errors appears, but this is a thing that you must ponderate about.
-	 * 
+	 *
 	 * @param string $value
-	 * @param string $tag 
+	 * @param string $tag
 	 */
-	function warning($value = '', $tag = self::DEFAULT_TAG) {
+	public function warning($value = '', $tag = self::DEFAULT_TAG) {
 
 		self::log('WARNING', $value, $tag);
 	}
@@ -115,11 +115,11 @@ class MyLogPHP {
 	 * Function to write ERROR messages that will be written into $LOGFILENAME.
 	 *
 	 * These messages are for fatal errors. Your script will NOT work properly if an ERROR happens, right?
-	 * 
+	 *
 	 * @param string $value
-	 * @param string $tag 
+	 * @param string $tag
 	 */
-	function error($value = '', $tag = self::DEFAULT_TAG) {
+	public function error($value = '', $tag = self::DEFAULT_TAG) {
 
 		self::log('ERROR', $value, $tag);
 	}
@@ -128,13 +128,27 @@ class MyLogPHP {
 	 * Function to write DEBUG messages that will be written into $LOGFILENAME.
 	 *
 	 * DEBUG messages are for variable values and other technical issues.
-	 * 
+	 *
 	 * @param string $value
-	 * @param string $tag 
+	 * @param string $tag
 	 */
-	function debug($value = '', $tag = self::DEFAULT_TAG) {
+	public function debug($value = '', $tag = self::DEFAULT_TAG) {
 
 		self::log('DEBUG', $value, $tag);
 	}
+
+	/**
+	 * Function to write the print_r return value to a file.
+	 *
+	 * Allow to append a variable value to the end of the file _OUT_MyLogPHP.txt
+	 *
+	 * @param mixed $variable_to_output
+	 * @param string $output_path
+	 */
+	public function out($variable_to_output, $output_path = "") {
+
+		file_put_contents($output_path . '_OUT_MyLogPHP.txt', print_r($variable_to_output, true), FILE_APPEND);
+	}
+
 }
 ?>
