144af73c58946def6ffbee09c186b7dfab8169aa
Lawrence Lagerlof <lawrence@ead.fiocruz.br>
2017-11-24 16:51:57 -0200

    Added a configuration file with an option to disable logging.

--- MyLogPHP.class.php
+++ MyLogPHP.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * MyLogPHP 1.2.15
+ * MyLogPHP 1.2.16
  *
  * MyLogPHP is a single PHP class to easily keep log files in CSV format.
  *
@@ -68,28 +68,30 @@ class MyLogPHP {
 	 */
 	private function log($errorlevel = 'INFO', $value = '', $tag) {
 
-		$datetime = date("Y-m-d H:i:s");
-		if (!file_exists($this->LOGFILENAME)) {
-			$headers = $this->HEADERS . "\n";
+		if (self::enabled()) {
+			$datetime = date("Y-m-d H:i:s");
+			if (!file_exists($this->LOGFILENAME)) {
+				$headers = $this->HEADERS . "\n";
+			}
+
+			$fd = fopen($this->LOGFILENAME, "a");
+
+			if (!empty($headers)) {
+				fwrite($fd, $headers);
+			}
+
+			$debugBacktrace = debug_backtrace();
+			$line = $debugBacktrace[1]['line'];
+			$file = $debugBacktrace[1]['file'];
+
+			$value = preg_replace('/\s+/', ' ', trim($value));
+
+			$entry = array($datetime,$errorlevel,$tag,$value,$line,$file);
+
+			fputcsv($fd, $entry, $this->SEPARATOR);
+
+			fclose($fd);
 		}
-
-		$fd = fopen($this->LOGFILENAME, "a");
-
-		if (!empty($headers)) {
-			fwrite($fd, $headers);
-		}
-
-		$debugBacktrace = debug_backtrace();
-		$line = $debugBacktrace[1]['line'];
-		$file = $debugBacktrace[1]['file'];
-
-		$value = preg_replace('/\s+/', ' ', trim($value));
-
-		$entry = array($datetime,$errorlevel,$tag,$value,$line,$file);
-
-		fputcsv($fd, $entry, $this->SEPARATOR);
-
-		fclose($fd);
 	}
 
 	/**
@@ -152,50 +154,66 @@ class MyLogPHP {
 	 * @param mixed $options
 	 */
 	public static function out($variable_to_output, $options = null) {
-		// $options = array('OUTPUT_PATH' => '', 'WRITE_MODE' => APPEND, 'LABEL' => 'Label');
-		// If OUTPUT_PATH is not defined, $_SESSION['MYLOGPHP_OUT_PATH'] is used instead. If the last is not defined too, current directory is used.
-		if (empty($options)) {
-			$write_mode = FILE_APPEND;
-			$output_path = (!empty($_SESSION['MYLOGPHP_OUT_PATH'])) ? $_SESSION['MYLOGPHP_OUT_PATH'] : '';
-			$label = null;
-		} elseif (is_string($options)) {
-			$write_mode = FILE_APPEND;
-			$output_path = (!empty($_SESSION['MYLOGPHP_OUT_PATH'])) ? $_SESSION['MYLOGPHP_OUT_PATH'] : '';
-			$label = $options;
-		} elseif (is_array($options)) {
-			$output_path = array_key_exists('OUTPUT_PATH', $options) ? $options['OUTPUT_PATH'] : ( (!empty($_SESSION['MYLOGPHP_OUT_PATH'])) ? $_SESSION['MYLOGPHP_OUT_PATH'] : '' );
-			$write_mode = array_key_exists('WRITE_MODE', $options) ? $options['WRITE_MODE'] : FILE_APPEND;
-			if ($write_mode !== FILE_APPEND) {
-				$write_mode = null;
+		if (self::enabled()) {
+			// $options = array('OUTPUT_PATH' => '', 'WRITE_MODE' => APPEND, 'LABEL' => 'Label');
+			// If OUTPUT_PATH is not defined, $_SESSION['MYLOGPHP_OUT_PATH'] is used instead. If the last is not defined too, current directory is used.
+			if (empty($options)) {
+				$write_mode = FILE_APPEND;
+				$output_path = (!empty($_SESSION['MYLOGPHP_OUT_PATH'])) ? $_SESSION['MYLOGPHP_OUT_PATH'] : '';
+				$label = null;
+			} elseif (is_string($options)) {
+				$write_mode = FILE_APPEND;
+				$output_path = (!empty($_SESSION['MYLOGPHP_OUT_PATH'])) ? $_SESSION['MYLOGPHP_OUT_PATH'] : '';
+				$label = $options;
+			} elseif (is_array($options)) {
+				$output_path = array_key_exists('OUTPUT_PATH', $options) ? $options['OUTPUT_PATH'] : ( (!empty($_SESSION['MYLOGPHP_OUT_PATH'])) ? $_SESSION['MYLOGPHP_OUT_PATH'] : '' );
+				$write_mode = array_key_exists('WRITE_MODE', $options) ? $options['WRITE_MODE'] : FILE_APPEND;
+				if ($write_mode !== FILE_APPEND) {
+					$write_mode = null;
+				}
+				$label = array_key_exists('LABEL', $options) ? $options['LABEL'] : null;
 			}
-			$label = array_key_exists('LABEL', $options) ? $options['LABEL'] : null;
+
+			$backtrace = debug_backtrace();
+			$backtrace_text = print_r($backtrace, true);
+			$filename_full = $backtrace[0]['file'];
+			$line_call = ' (' . $backtrace[0]['line'] . ')';
+			$method_call = (!empty($backtrace[1]['function'])) ? $backtrace[1]['function'] . '() :: ' : '';
+
+			$arr_file_name = explode('(', basename($filename_full));
+			$file_name = str_repeat(' ', 80 - strlen($method_call . $arr_file_name[0] . $line_call)) . $method_call . $arr_file_name[0] . $line_call;
+
+			$print_r_variable_to_output = print_r($variable_to_output, true) . "\n\n" . str_repeat('-', 80) . "\n";
+
+			$variable_type = gettype($variable_to_output);
+
+			$datetime = date("H:i:s Y-m-d");
+
+			if (!empty($label)) {
+				$datetime_span_position = 80 - strlen($datetime);
+				$print_r_variable_to_output = $file_name . "\n" . str_repeat(' ', $datetime_span_position) . $datetime . "\n" . '[' . $label . '] ' . $variable_type . "\n\n" . $print_r_variable_to_output;
+			} else {
+				$datetime_span_position = 80 - strlen($datetime);
+				$print_r_variable_to_output = $file_name . "\n" . $datetime . "\n" . $variable_type . "\n\n" . $print_r_variable_to_output;
+			}
+
+			file_put_contents($output_path . '_OUT_MyLogPHP.txt', $print_r_variable_to_output, $write_mode);
 		}
-
-		$backtrace = debug_backtrace();
-		$backtrace_text = print_r($backtrace, true);
-		$filename_full = $backtrace[0]['file'];
-		$line_call = ' (' . $backtrace[0]['line'] . ')';
-		$method_call = (!empty($backtrace[1]['function'])) ? $backtrace[1]['function'] . '() :: ' : '';
-
-		$arr_file_name = explode('(', basename($filename_full));
-		$file_name = str_repeat(' ', 80 - strlen($method_call . $arr_file_name[0] . $line_call)) . $method_call . $arr_file_name[0] . $line_call;
-
-		$print_r_variable_to_output = print_r($variable_to_output, true) . "\n\n" . str_repeat('-', 80) . "\n";
-
-		$variable_type = gettype($variable_to_output);
-
-		$datetime = date("H:i:s Y-m-d");
-
-		if (!empty($label)) {
-			$datetime_span_position = 80 - strlen($datetime);
-			$print_r_variable_to_output = $file_name . "\n" . str_repeat(' ', $datetime_span_position) . $datetime . "\n" . '[' . $label . '] ' . $variable_type . "\n\n" . $print_r_variable_to_output;
-		} else {
-			$datetime_span_position = 80 - strlen($datetime);
-			$print_r_variable_to_output = $file_name . "\n" . $datetime . "\n" . $variable_type . "\n\n" . $print_r_variable_to_output;
-		}
-
-		file_put_contents($output_path . '_OUT_MyLogPHP.txt', $print_r_variable_to_output, $write_mode);
 	}
 
+	private static function enabled() {
+		if (file_exists(dirname(__FILE__) . '/MyLogPHP.conf')) {
+			$contents = file(dirname(__FILE__) . '/MyLogPHP.conf');
+			foreach ($contents as $line) {
+				$config_value = explode('=', $line);
+				if (($config_value[0] == "enabled") && (trim($config_value[1]) == "false")) {
+					return false;
+				}
+			}
+
+		}
+		return true;
+	}
 }
+
 ?>
